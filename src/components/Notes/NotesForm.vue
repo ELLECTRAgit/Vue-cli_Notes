f257c032eadce94c43e9bcba0155e1c1c1666ad7
Любовь Сергеевна Анохина <Anokhina_l@AZARD.local>
2024-10-21 08:20:59 +0300

    add notes

---
+++ src/components/Notes/NotesForm.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="note-form__wrapper">
+    <form class="note-form" @submit.prevent="onSubmit">
+      <textarea v-model="value" placeholder="Сделайте запись" />
+      <TagsList :items="tags" @onItemClick="handlerTags" />
+      <button class="btn btnPimary" type="submit">Добавьте запись</button>
+    </form>
+  </div>
+</template>
+<script>
+import TagsList from '@/components/UI/TagsList.vue';
+export default {
+  components: {
+    TagsList,
+  },
+  data() {
+    return {
+      value: '',
+      tags: ['nome', 'work', 'travel'],
+    };
+  },
+  methods: {
+    onSubmit() {
+      this.$emit('onSubmit', this.value);
+      console.log(this.value);
+      this.value = '';
+    },
+    handlerTags(tag) {
+      console.log(tag);
+    },
+  },
+};
+</script>
+<style>
+.note-form {
+  max-width: 600px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+}
+.note-form__wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
--- src/pages/TheHome.vue
+++ src/pages/TheHome.vue
@@ -1,5 +1,29 @@
 <template>
-    <p>Hello Home</p>
+  <Form @onSubmit="handleSubmit" />
+  <List @onRemove="handleRemove" :items="notes" />
 </template>
 
+<script>
+import Form from '@/components/Notes/NotesForm';
+import List from '@/components/Notes/NotesList';
 
+export default {
+  data() {
+    return {
+      notes: ['task 1', 'task 2', 'task 3'],
+    };
+  },
+  components: {
+    Form,
+    List,
+  },
+  methods: {
+    handleSubmit(note) {
+      this.notes.push(note);
+    },
+    handleRemove(index) {
+      this.notes.splice(index, 1);
+    },
+  },
+};
+</script>
