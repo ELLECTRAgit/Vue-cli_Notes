958e832a1fdff43348b0a90d86aec97909d61339
Ellectra <ellectra@bk.ru>
2024-10-23 14:29:07 +0300

    init about

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -2,13 +2,21 @@
     <footer class="footer">
         <div class="container">
                 <ul class="navbar-list">
-                    <li class="navbar-item">
-                        <a href="/" class="navbar-link">Home</a>
-                    </li>
-                    <li class="navbar-item">
-                        <a href="/about" class="navbar-link">About</a>
+                    <li class="navbar-item" v-for="link in links" :key="link.alias">
+                        <router-link :to="link.url" class="navbar-link">{{link.title}}</router-link>
                     </li>
                 </ul>
         </div>
     </footer>
 </template>
+<script>
+import {app, links} from "@/config";
+export default {
+    data () {
+        return {
+            links: links,
+            title: app.title,
+        }
+    }
+};
+</script>
